4f2771155aa91ed4d13a4c1a4ded8de95aaa7a1c
sujith kumar <148097955+sujith52@users.noreply.github.com>
2025-05-04 08:54:22 +0530

    Update session6.py

--- session6.py
+++ session6.py
@@ -1,39 +1,39 @@
 # Session 5: Loops – While Loops and Nested Loops
 # 12:39 it was a haard session
 
-# count = 10
-# while count>=1:
-#     print(count)
-#     count -= 1
-# print("Lift off")
+count = 10
+while count>=1:
+    print(count)
+    count -= 1
+print("Lift off")
 
-#       today 04-05-2025
-# 15 percent of the data analysit course i completed now python 
+      today 04-05-2025
+15 percent of the data analysit course i completed now python 
 
 #   challenge 2
-# num = 1
-# total =0
-# while num <= 400:
-#     if num % 2 == 0:
-#         total += num
-#     num += 1
-# print("The sum of nums from 1 to 50 are ;",total)
+num = 1
+total =0
+while num <= 400:
+    if num % 2 == 0:
+        total += num
+    num += 1
+print("The sum of nums from 1 to 50 are ;",total)
 
 #   challeng 3
-# for i in range (1,5):
-#     for j in range(1,11):
-#         print(i,'x',j,'=',i*j)
-#     print("------------")
+for i in range (1,5):
+    for j in range(1,11):
+        print(i,'x',j,'=',i*j)
+    print("------------")
 
 #   challenge 4
-# i = 1
-# while i<=4:
-#     j=1
-#     while j<= i:
-#         print('*',end=" ")
-#         j+=1
-#     print()
-#     i+=1
+i = 1
+while i<=4:
+    j=1
+    while j<= i:
+        print('*',end=" ")
+        j+=1
+    print()
+    i+=1
 
 #   challenge 5
 
